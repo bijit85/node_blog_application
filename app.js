b1303d49c57c5f7054875dfffcaa5c06d49bec2b
Bijit Kundu <bijit.kundu@yahoo.com>
2021-12-30 19:37:24 +0800

    design corrections for each post

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.locals.moment = require('moment');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
